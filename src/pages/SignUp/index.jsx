b824edcc54963afd023b4eefb693bdabf77d5c1b
Wilmar dos Santos <dswilmar@gmail.com>
2023-02-01 21:09:08 -0300

    feat: tela de cadastro

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,10 +1,36 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.png';
+
 function SignUp() {
-    return (
-      <div>
-        <h1>Cadastro</h1>
-      </div>
-    );
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert('Clicou');
   }
+
+  return (
+    <div className="container-center">
+      <div className="login">
+        <div className="logo-area">
+          <img src={ logo } alt="Logo do Sistema" />
+        </div>
+        <form onSubmit={handleSubmit}>
+          <h1>Crie sua conta</h1>
+          <input type="text" placeholder="Insira seu nome" value={name} onChange={(e) => setName(e.target.value)} autoFocus required />
+          <input type="email" placeholder="Insira seu e-mail" value={email} onChange={(e) => setEmail(e.target.value)} required />
+          <input type="password" placeholder="Insira sua senha" value={password} onChange={(e) => setPassword(e.target.value)} required />
+          <button type="submit">Cadastrar</button>
+        </form>
+        <Link to="/">Já possui uma conta? Faça login</Link>
+      </div>
+    </div>
+  );
+}
   
-  export default SignUp;
+export default SignUp;
   
